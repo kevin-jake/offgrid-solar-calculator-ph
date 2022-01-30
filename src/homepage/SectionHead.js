25f2efecc9d5db1022dee72904ef6373914323f0
Kevin Jake Casala <kevin_jake_04@yahoo.com>
2022-01-31 03:29:44 +0800

    make a cleaner look ui

--- src/homepage/SectionHead.js
+++ src/homepage/SectionHead.js
@@ -1,10 +1,8 @@
 import React from "react";
 
-import "./Styling.scss";
-
 const SectionHead = (props) => {
   return (
-    <div className={`pricing-table-1 ${props.className}`} style={props.style}>
+    <div className={`${props.className}`} style={props.style}>
       {props.children}
     </div>
   );
