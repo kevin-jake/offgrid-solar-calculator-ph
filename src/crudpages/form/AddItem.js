942b0e1b4653ffad91314fecad1576698f942a7a
Kevin Jake Casala <kevin_jake_04@yahoo.com>
2022-02-10 10:50:32 +0800

    fix validation rerenders

--- src/crudpages/form/AddItem.js
+++ src/crudpages/form/AddItem.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 import Backdrop from "../../shared/components/UIElements/Backdrop";
@@ -7,6 +7,7 @@ import { LOVContext } from "../../homepage/tabs/context/lov-context";
 import { validate } from "../../shared/util/validators";
 
 const AddItemOverlay = ({ onCancel, formInputs, title, state }) => {
+  const [content, setContent] = useState(<></>);
   const {
     inverters,
     batterylist,
@@ -24,6 +25,10 @@ const AddItemOverlay = ({ onCancel, formInputs, title, state }) => {
   const [dataState, setDataState] = useState({});
   const [validState, setValidState] = useState(state);
 
+  useEffect(() => {
+    setContent(renderContent());
+  }, [validState, dataState]);
+
   const handleItemChanged = (event, objkey, data) => {
     data[objkey] = event.value;
     setSelectedState(event);
@@ -38,20 +43,25 @@ const AddItemOverlay = ({ onCancel, formInputs, title, state }) => {
       console.log(val);
       const valid = validate(val, validator);
       if (valid) {
+        const stateSet = validState;
+        stateSet[objkey] = true;
+        setValidState(stateSet);
         setDataState(data);
+        setContent(renderContent());
         console.log(dataState);
       } else {
         const stateSet = validState;
         stateSet[objkey] = false;
         setValidState(stateSet);
+        setContent(renderContent());
         console.log(validState);
       }
     } else setDataState(data);
   };
 
   const handleSave = (event, data, title) => {
+    setContent(renderContent());
     event.preventDefault();
-
     switch (title) {
       case "Battery": {
         let newlist = batterylist;
@@ -188,58 +198,60 @@ const AddItemOverlay = ({ onCancel, formInputs, title, state }) => {
       </div>
     );
   };
-  const content = (
-    <div
-      id="authentication-modal"
-      className="fixed right-0 left-0 top-4 bottom-4 z-50 justify-center items-center h-modal md:h-full md:inset-0 flex"
-      aria-modal="true"
-      role="dialog"
-    >
-      <div className="relative px-4 w-full max-w-4xl h-full md:h-auto overflow-y-auto max-h-screen">
-        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-          <div className="flex justify-end p-2">
-            <button
-              type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-              onClick={onCancel}
-            >
-              <svg
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+  const renderContent = () => {
+    return (
+      <div
+        id="childcontent"
+        className="fixed right-0 left-0 top-4 bottom-4 z-50 justify-center items-center h-modal md:h-full md:inset-0 flex"
+        aria-modal="true"
+        role="dialog"
+      >
+        <div className="relative px-4 w-full max-w-4xl h-full md:h-auto overflow-y-auto max-h-screen">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="flex justify-end p-2">
+              <button
+                type="button"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                onClick={onCancel}
               >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-          </div>
-          <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
-            <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
-              Add New {title}
-            </h2>
-
-            <form onSubmit={(e) => handleSave(e, dataState, title)}>
-              <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-                {formInputs.map((obj) => renderInputs(obj))}
-              </div>
-              <div className="flex justify-end mt-6">
-                <button
-                  type="submit"
-                  className="block px-5 py-2 mt-5 font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg lg:mt-0 hover:bg-blue-500 lg:w-auto"
+                <svg
+                  className="w-5 h-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  Save
-                </button>
-              </div>
-            </form>
-          </section>
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+              <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+                Add New {title}
+              </h2>
+
+              <form onSubmit={(e) => handleSave(e, dataState, title)}>
+                <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+                  {formInputs.map((obj) => renderInputs(obj))}
+                </div>
+                <div className="flex justify-end mt-6">
+                  <button
+                    type="submit"
+                    className="block px-5 py-2 mt-5 font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg lg:mt-0 hover:bg-blue-500 lg:w-auto"
+                  >
+                    Save
+                  </button>
+                </div>
+              </form>
+            </section>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
   return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
 };
 
