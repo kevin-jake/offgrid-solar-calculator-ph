2e5a003a9b59e06a9223b09a799ad8c564cbb1d5
Kevin Jake Casala <kevin_jake_04@yahoo.com>
2022-03-01 21:23:02 +0800

    add voktage load on saved data

--- src/homepage/Home.js
+++ src/homepage/Home.js
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import MainCalculation from "./MainCalculation";
 import "./Home.css";
 import NavigateCalculations from "./NavigateCalculations";
-import { useHttpClient } from "../shared/components/hooks/http-hook";
 
 const Home = () => {
   return (
